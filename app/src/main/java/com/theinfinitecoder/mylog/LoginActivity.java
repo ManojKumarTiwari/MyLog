849f511e90cee8bedfeecd16818d616469cf09c7
Manoj Kumar Tiwari <manojtiwari1.1v@gmail.com>
2018-02-02 11:11:07 +0530

    third commit -Imported to ttf files -Used ttf files to change font of welcome log

--- app/src/main/java/com/theinfinitecoder/mylog/LoginActivity.java
+++ app/src/main/java/com/theinfinitecoder/mylog/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.theinfinitecoder.mylog;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView welcomeTx = (TextView) findViewById(R.id.login_welcome_tx);
+        TextView myLogTx = (TextView) findViewById(R.id.login_mylog_tx);
+
+        Typeface alexBrushRegular = Typeface.createFromAsset(getAssets(), "fonts/AlexBrush-Regular.ttf");
+        welcomeTx.setTypeface(alexBrushRegular);
+
+        Typeface robotoSlabLight = Typeface.createFromAsset(getAssets(), "fonts/RobotoSlab-Light.ttf");
+        myLogTx.setTypeface(robotoSlabLight);
     }
 }
